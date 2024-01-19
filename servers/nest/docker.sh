c131803473dbe468e40e55d5c2cb82307140a772
choewy <choewy32@gmail.com>
2024-01-19 18:39:24 +0900

    feature/servers nest docker

---
+++ servers/nest/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+PORT="$1"
+NAME="nest"
+
+if [ "$PORT" == "" ]; then
+  PORT="3000"
+fi
+
+docker build . -t $NAME
+
+if [ "$(docker container inspect --format '{{.Name}}' $NAME 2>&1)" == "/$NAME" ]; then
+  docker rm -f $NAME
+fi
+
+docker run --name $NAME -d -p $PORT:3000 $NAME
+
+exit 0
