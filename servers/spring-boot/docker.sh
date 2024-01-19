77da2fb95482263e5ce7fd3f68929cc5f2acb3ed
choewy <choewy32@gmail.com>
2024-01-19 18:20:05 +0900

    feature/servers spring boot docker

---
+++ servers/spring-boot/docker.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+PORT="$1"
+NAME="springboot"
+
+if [ "$PORT" == "" ]; then
+  PORT="3000"
+fi
+
+sh gradlew clean build
+
+docker build . -t $NAME
+
+if [ "$(docker container inspect --format '{{.Name}}' $NAME 2>&1)" == "/$NAME" ]; then
+  docker rm -f $NAME
+fi
+
+docker run --name $NAME -d -p $PORT:3000 $NAME
+
+exit 0
